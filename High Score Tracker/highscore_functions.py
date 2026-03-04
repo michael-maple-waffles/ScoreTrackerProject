5bd12de00c6ef64d3fc269a07e7993acea2528cc
michael-maple-waffles <michael.willes@ucas-edu.net>
2026-03-04 17:51:09 +0000

    I am pissed as always!!!

--- High Score Tracker/highscore_functions.py
+++ High Score Tracker/highscore_functions.py
@@ -2,39 +2,71 @@
 
 import csv
 
-def submittingScores(username, scores_csv):
+def gettingScores(username, scores_csv, mode):
     #try
-    try:
-        #with open scores_csv, mode = r as scores_by_username:
-        with open(scores_csv, 'r') as scores_by_username:
-            #reader = csv.reader(scores_by_username)
-            reader = csv.reader(scores_by_username)
-            user_scores = {}
-            #for row in reader:
-            next(reader)
-            for i, row in enumerate(reader):
-                print(f"Row # {i}, {row}")
-                #scores = []
-                scores = []
-                #scores = row[1].split(',')
-                current_item = ''
-                for char in row[1]:
-                    if char != '[' or ',' or ' ':
-                        current_item = f'{current_item}{char}'
-                    else:
-                        if current_item == '':
-                            pass
+    def reading():
+        try:
+            #with open scores_csv, mode = r as scores_by_username:
+            with open(scores_csv, 'r') as scores_by_username:
+                #reader = csv.reader(scores_by_username)
+                reader = csv.reader(scores_by_username)
+                user_scores = {}
+                #for row in reader:
+                next(reader)
+                for i, row in enumerate(reader):
+                    #scores = []
+                    scores = []
+                    #scores = row[1].split(',')
+                    current_item = ''
+                    workable_row = row[1].replace('[', '').replace(']','')
+                    for char in workable_row:
+                        if char != '[' and char!= '.' and char!= ' ':
+                            current_item = f'{current_item}{char}'
                         else:
-                            scores.append(current_item)
-                            current_item = ''
+                            if current_item == '':
+                                pass
+                            else:
+                                
+                                scores.append(current_item)
+                                current_item = ''
+                        
+                    scores.append(current_item)
+                    user_scores[row[0]] = scores
 
-                print(scores)
-                user_scores[row[0]] = scores
-
-    except:
-        print("file doesn't exist")
-    else:
-        return user_scores[username]
+        except:
+            print("file doesn't exist")
+        else:
+            return user_scores
     
-print(submittingScores("PersonOne", "/workspaces/ScoreTrackerProject/documents/scores.csv"))
+    if mode == 'spec':
+        return reading()[username]
+    elif mode == 'all':
+        return reading()
+    
+def submittingScores(username, new_scores):
+    read_info = gettingScores('NULL', '/workspaces/ScoreTrackerProject/documents/scores.csv', 'all')
+    read_info[username].append(new_scores)
+    writeable_info = []
+    for item in read_info:
+        #print(item)
+        #piece = {'Username' : item}
+        pass
+
+
+    """try:
+        with open('/workspaces/ScoreTrackerProject/documents/scores.csv', 'w'):
+            feildnames = ['Username', 'Scores']
+            writer = csv.DictWriterwriter()"""
+    
+
+
+submittingScores('PersonOne', [12,17])
+            
+
+            
+        
+    
+print(gettingScores("PersonOne", "/workspaces/ScoreTrackerProject/documents/scores.csv", 'spec'))
+
+
 
---
+++ High Score Tracker/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    """try:
+            with open('/workspaces/ScoreTrackerProject/documents/scores.csv', 'w'):
+                feildnames = ['Username', 'Scores']
+                writer = csv.DictWriterwriter()"""
